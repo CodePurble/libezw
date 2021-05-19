5f124cae0c4acf2c5c740a591d23cf7a62a5f53e
Ramprakash Chandrashekar <49452779+CodePurble@users.noreply.github.com>
2021-05-19 17:28:10 +0530

    Refactor main * Filenames are now read as command-line args * Handle exits better * Parse command-line args properly * Some general identifier renaming

--- src/utils.c
+++ src/utils.c
@@ -1,11 +1,13 @@
 #include "utils.h"
 
-void read_binary_file(FILE *infile, unsigned char* dest, int rows, int cols)
+void read_binary_file(FILE *infile, unsigned char *dest_arr, int rows, int cols)
 {
-    fread(dest, 1, rows*cols, infile);
+    printf("Reading binary file...\n");
+    fread(dest_arr, 1, rows*cols, infile);
 }
 
-void write_binary_file(FILE *outfile, unsigned char *src, int rows, int cols)
+void write_binary_file(FILE *outfile, unsigned char *src_arr, int rows, int cols)
 {
-    fwrite(src, 1, rows*cols, outfile);
+    printf("Writing binary file...\n");
+    fwrite(src_arr, 1, rows*cols, outfile);
 }
