3040ae2c94e28249dea273bbffa4eb088e049553
Ramprakash Chandrashekar <49452779+CodePurble@users.noreply.github.com>
2021-05-20 08:26:18 +0530

    Add utils for type conversion of arrays * Convert array of doubles to array of uchars * Test `idwt2` function
    Add next TODO

--- include/utils.h
+++ include/utils.h
@@ -4,6 +4,6 @@
 
 void read_binary_file(FILE *infile, unsigned char *dest_arr, int rows, int cols);
 void write_binary_file(FILE *outfile, unsigned char *src_arr, int rows, int cols);
-void peek_binary_file();
+void double_to_uchar(double *src, unsigned char* dest, int rows, int cols);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -11,3 +11,12 @@ void write_binary_file(FILE *outfile, unsigned char *src_arr, int rows, int cols
     printf("Writing binary file...\n");
     fwrite(src_arr, 1, rows*cols, outfile);
 }
+
+void double_to_uchar(double *src, unsigned char* dest, int rows, int cols)
+{
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            dest[i*cols + j] = (unsigned char) src[i*cols + j];
+        }
+    }
+}
