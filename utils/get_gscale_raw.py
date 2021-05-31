7eafd8b46639c5be410acb15d16e94c4b5ac8ba4
Ramprakash Chandrashekar <49452779+CodePurble@users.noreply.github.com>
2021-05-31 11:10:41 +0530

    get_gscale_raw now reads input files as args

--- utils/get_gscale_raw.py
+++ utils/get_gscale_raw.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 from PIL import Image
+from sys import argv
 
-img = Image.open('Lichtenstein_img_processing_test_bw.png').convert('L')
+ifname = argv[1]
+ofname = ifname + ".bin"
+
+img = Image.open(ifname).convert('L')
 WIDTH, HEIGHT = img.size
 
 data = list(img.getdata())
 data_bytes = bytes(img.getdata())
 #  data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
 
-with open("raw_lichtenstein_img_processing_test_bw.bin", "wb") as f:
+with open(ofname, "wb") as f:
     f.write(data_bytes)
 #  for row in data:
 #      for value in row:
