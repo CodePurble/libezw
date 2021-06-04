cd4cb816f3aebb059e2e162e0d3501c38df9fa68
Ramprakash Chandrashekar <49452779+CodePurble@users.noreply.github.com>
2021-06-04 08:13:49 +0530

    memory corruption by secret double free fixed

--- src/ezw.c
+++ src/ezw.c
@@ -9,11 +9,11 @@ SBtree_node* sb_treeify(int levels, double* input, int rows, int cols)
     wave_object w_obj;
     w_obj = wave_init("haar");
     wt2_object* wt2_arr = (wt2_object *) calloc(sizeof(wt2_object), levels);
-    double* subcoeffs_A; // LL
-    double* subcoeffs_H; // HL
-    double* subcoeffs_V; // LH
-    double* subcoeffs_D; // HH
-    double* curr_wavecoeffs;
+    double *subcoeffs_A; // LL
+    double *subcoeffs_H; // HL
+    double *subcoeffs_V; // LH
+    double *subcoeffs_D; // HH
+    double *curr_wavecoeffs;
     int sub_r, sub_c;
 
     for(int i = 0; i < levels; i++) {
@@ -33,7 +33,6 @@ SBtree_node* sb_treeify(int levels, double* input, int rows, int cols)
     }
 
     // clean up
-    free(curr_wavecoeffs);
     wave_free(w_obj);
     for(int i = 0; i < levels; i++) {
         wt2_free(wt2_arr[i]);
